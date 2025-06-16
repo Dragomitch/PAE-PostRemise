091394fd6a2f8dffdda27fa74507d00d5ae9ebf2
DragomitchCode <Dragomitch@users.noreply.github.com>
2025-06-16 13:33:17 +0200

    Introduce Spring Boot skeleton

---
+++ src/main/java/com/dragomitch/ipl/pae/AppConfig.java
@@ -0,0 +1,9 @@
+package com.dragomitch.ipl.pae;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.dragomitch.ipl.pae")
+public class AppConfig {
+}
